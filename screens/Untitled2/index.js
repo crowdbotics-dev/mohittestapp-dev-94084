b2831961cf2319a18cb237e978280642d5ee2b19
Ravinder sales <ravinder.y@crowdbotics.com>
2023-06-23 12:37:00 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,31 +1,43 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable, Image, StyleSheet } from 'react-native';
+import { api_v1_login_create } from "../../store/mohittestappdevAPI/authTokens.slice.js";
+import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { View, Text, TextInput, Pressable, Image, StyleSheet } from "react-native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {// Handle login logic here
   };
 
   return <View style={styles.container}>
-      <Image source={{
-      uri: 'https://tinyurl.com/42evm3m3'
-    }} style={styles.logo} />
+      <Pressable onPress={onSubmit}>
+        <Image source={{
+        uri: "https://tinyurl.com/42evm3m3"
+      }} style={styles.logo} />
+      </Pressable>
       <TextInput style={styles.input} onChangeText={setEmail} value={email} placeholder="Email" keyboardType="email-address" autoCapitalize="none" />
       <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
       <Pressable style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
       </Pressable>
     </View>;
+
+  const onSubmit = () => {
+    dispatch(api_v1_login_create({
+      username: email,
+      password
+    }));
+  };
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   logo: {
     width: 150,
@@ -33,23 +45,23 @@ const styles = StyleSheet.create({
     marginBottom: 30
   },
   input: {
-    width: '80%',
+    width: "80%",
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 15
   },
   button: {
-    backgroundColor: '#1E90FF',
+    backgroundColor: "#1E90FF",
     borderRadius: 5,
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginBottom: 15
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16
   }
 });
